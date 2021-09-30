6a28bb32966ec185a7893083857bb0f8c3ab565c
Lavoxs <65963206+Lavoxs@users.noreply.github.com>
2021-09-30 17:49:33 +0200

    Translation completed + DarkMode refined
    (need a solution to a deprecated method in Language Manager)

--- app/src/main/java/com/identitymanager/activities/MainActivity.java
+++ app/src/main/java/com/identitymanager/activities/MainActivity.java
@@ -1,18 +1,27 @@
 package com.identitymanager.activities;
 
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.identitymanager.R;
+import com.identitymanager.shared.LanguageManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences.Editor editorLanguage;
+    SharedPreferences.Editor editorTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         int idFragment = bundle.getInt("fragment");
+        int idLoad = bundle.getInt("load", 0);
+
+        SharedPreferences sharedLanguage = getSharedPreferences("language", 0);
+        int refresh = sharedLanguage.getInt("refresh", 0);
+        editorLanguage = sharedLanguage.edit();
+
+        SharedPreferences sharedTheme = getSharedPreferences("color", 0);
+        int theme = sharedTheme.getInt("theme", 0);
+        editorTheme = sharedTheme.edit();
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnItemSelectedListener(navListener);
@@ -39,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
                 view.performClick();
                 break;
             case 4:
-                fragment = new SettingsActivity();
                 view = bottomNav.findViewById(R.id.nav_settings);
                 view.performClick();
                 break;
@@ -47,15 +64,33 @@ public class MainActivity extends AppCompatActivity {
                 fragment = new DashboardActivity();
                 view = bottomNav.findViewById(R.id.nav_dashboard);
                 view.performClick();
+
+                if (idLoad == 0) {
+                    identifyModePreference(theme);
+                    getIntent().putExtra("load", 1);
+                    recreate();
+                }
+                if (theme == 2) {
+                    darkModeActionBar();
+                }
+                if (idLoad == 1) {
+                    identifyLanguagePreference(refresh);
+                    getIntent().putExtra("load", 2);
+                    recreate();
+                }
                 break;
         }
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+        if (idFragment != 4) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+        }
     }
 
     private  NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Bundle bundle = getIntent().getExtras();
+            int idChange = bundle.getInt("change_value");
             Fragment selectedFragment = null;
 
             switch (item.getItemId()) {
@@ -72,13 +107,68 @@ public class MainActivity extends AppCompatActivity {
                      selectedFragment = new SettingsActivity();
                      break;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            if (idChange != 4) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+
+            restoreChangeValue();
 
             return true;
         }
     };
 
-    public void darkModeColours () {
+   @Override
+    public Resources.Theme getTheme() {
+        Resources.Theme theme = super.getTheme();
+        theme.applyStyle(R.style.DarkTheme, true);
+        return theme;
+    }
+
+    public void darkModeActionBar() {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(getApplicationContext(), R.color.purple_500)));
+    }
+
+    public void restoreChangeValue() {
+        getIntent().putExtra("change_value", 0);
+    }
+
+    public void identifyLanguagePreference(int refresh) {
+        if (refresh == 1) {
+            LanguageManager lang = new LanguageManager(getBaseContext());
+            lang.updateResources("en");
+        } else if (refresh == 2) {
+            LanguageManager lang = new LanguageManager(getBaseContext());
+            lang.updateResources("it");
+        }
+    }
+
+    public void setChangeLanguageEnglish() {
+        editorLanguage.putInt("refresh", 1);
+        editorLanguage.commit();
+    }
+
+    public void setChangeLanguageItalian() {
+        editorLanguage.putInt("refresh", 2);
+        editorLanguage.commit();
+    }
+
+    public void identifyModePreference(int theme) {
+        if (theme == 1) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (theme == 2) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            setTheme(R.style.DarkTheme);
+        }
+    }
+
+    public void setLightTheme() {
+        editorTheme.putInt("theme", 1);
+        editorTheme.commit();
+    }
+
+    public void setDarkTheme() {
+        editorTheme.putInt("theme", 2);
+        editorTheme.commit();
     }
 }
--- app/src/main/java/com/identitymanager/activities/SettingsActivity.java
+++ app/src/main/java/com/identitymanager/activities/SettingsActivity.java
@@ -1,10 +1,13 @@
 package com.identitymanager.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,35 +15,101 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
+import com.identitymanager.shared.LanguageManager;
 import com.identitymanager.R;
 
 public class SettingsActivity extends Fragment {
 
-    private SwitchCompat aSwitch;
+    SharedPreferences.Editor editor;
+    SharedPreferences.Editor editorRestart;
+    SharedPreferences.Editor editorMode;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View settFrag = inflater.inflate(R.layout.activity_settings,container,false);
+        View settView = inflater.inflate(R.layout.activity_settings, container, false);
 
-        darkMode(settFrag);
+        changeLanguage(settView);
+        darkMode(settView);
 
-        return settFrag;
+        return settView;
     }
 
-    public void darkMode(View settFrag) {
+    public void changeLanguage(View settView) {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("choose", 0);
+        int sP = sharedPreferences.getInt("sP", 1);
+        editor = sharedPreferences.edit();
 
-        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
-            getActivity().setTheme(R.style.DarkTheme);
-            ((MainActivity) getActivity()).darkModeColours();
-        } else {
-            getActivity().setTheme(R.style.Theme_IdentityManager);
+        SharedPreferences sharedRestart = getActivity().getSharedPreferences("choose", 0);
+        int count = sharedRestart.getInt("count", 0);
+        editorRestart = sharedRestart.edit();
+
+        RadioGroup radioGroup = settView.findViewById(R.id.radio_group);
+        RadioButton enRb = settView.findViewById(R.id.radio_button1);
+        RadioButton itRb = settView.findViewById(R.id.radio_button2);
+        LanguageManager lang = new LanguageManager(getContext());
+
+        if (count == 0) {
+            enRb.setChecked(true);
+            ((MainActivity) getActivity()).setChangeLanguageItalian();
         }
 
-        aSwitch = settFrag.findViewById(R.id.s1);
+        saveLanguagePreferences(sP, count, enRb, itRb, lang);
 
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.radio_button1) {
+                    lang.updateResources("en");
+                    editor.putInt("sP", 1);
+                } else {
+                    lang.updateResources("it");
+                    editor.putInt("sP", 2);
+                }
+                editor.commit();
+
+                editorRestart.putInt("count", 1);
+                editorRestart.commit();
+
+                remainInSettings();
+            }
+        });
+    }
+
+    public void saveLanguagePreferences(int sP, int count, RadioButton enRb, RadioButton itRb, LanguageManager lang) {
+        if (sP == 1 && count == 1) {
+            enRb.setChecked(true);
+            lang.updateResources("en");
+            ((MainActivity) getActivity()).setChangeLanguageEnglish();
+
+            editorRestart.putInt("count", 1);
+            editorRestart.commit();
+        } else if (sP == 2 && count == 1) {
+            itRb.setChecked(true);
+            lang.updateResources("it");
+            ((MainActivity) getActivity()).setChangeLanguageItalian();
+
+            editorRestart.putInt("count", 1);
+            editorRestart.commit();
+        }
+    }
+
+    public void darkMode(View settView) {
+        SharedPreferences sharedMode = getActivity().getSharedPreferences("mode", 0);
+        int check = sharedMode.getInt("check", 0);
+        editorMode = sharedMode.edit();
+
+        SwitchCompat aSwitch = settView.findViewById(R.id.s1);
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES || check == 2) {
             aSwitch.setChecked(true);
+            ((MainActivity) getActivity()).darkModeActionBar();
+            ((MainActivity) getActivity()).setDarkTheme();
         }
 
         aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -48,17 +117,29 @@ public class SettingsActivity extends Fragment {
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (aSwitch.isChecked()) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    ((MainActivity) getActivity()).setDarkTheme();
+                    editorMode.putInt("check", 2);
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    ((MainActivity) getActivity()).setLightTheme();
+                    editorMode.putInt("check", 1);
                 }
+                editorMode.commit();
 
-                remainInSettings();
+                remainInSettingsCoordinationLanguage();
             }
         });
     }
 
     public void remainInSettings() {
         getActivity().getIntent().putExtra("fragment", 4);
+        getActivity().getIntent().putExtra("change_value", 4);
+        getActivity().recreate();
+    }
+
+    public void remainInSettingsCoordinationLanguage() {
+        getActivity().getIntent().putExtra("fragment", 4);
+        getActivity().getIntent().putExtra("change_value", 0);
         getActivity().recreate();
     }
 }
